b7b57aa1983b60e64db8138c067b2a1ffe5fab4d
David2 <David2@David-PC>
2021-02-01 19:07:58 +0100

    Version 1.0

---
+++ Practica entornos Arrays 1/src/practica/entornos/arrays/pkg1/PracticaEntornosArrays1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.entornos.arrays.pkg1;
+
+/**
+ *
+ * @author David2
+ */
+public class PracticaEntornosArrays1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Empiezo programa");
+    }
+    
+}

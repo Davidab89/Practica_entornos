f3fbe72f6f7c503ab5b6bd558b0456a937293f93
David2 <David2@David-PC>
2021-02-08 00:01:32 +0100

    Version 1.5

--- Practica entornos Arrays 1/src/practica/entornos/arrays/pkg1/PracticaEntornosArrays1.java
+++ Practica entornos Arrays 1/src/practica/entornos/arrays/pkg1/PracticaEntornosArrays1.java
@@ -18,32 +18,38 @@ public class PracticaEntornosArrays1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int numerosLeer;
-        Scanner sc=new Scanner (System.in);
-        do{
-        System.out.println("¿Cuantos numeros desea leer?(Maximo 10):");
-        numerosLeer=sc.nextInt();
-        if (numerosLeer>10){
-            System.out.println("Error, el maximo es de 10 numeros.");
-        }
-        }while (numerosLeer>10);
-        int numeros[]=new int[numerosLeer];
-        
-        for(int i=0;i<numeros.length;i++){
-            System.out.println("Anota el numero "+i+":");
-            numeros[i]=sc.nextInt();
-            for(int contador=0;contador<i;contador++){
-                if (numeros[i]==numeros[contador]){
-                 System.out.println("Los numeros no pueden repetirse");
-                 i--;
+        int numerosLeer, numBuscar;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("¿Cuantos numeros desea leer?(Maximo 10):");
+            numerosLeer = sc.nextInt();
+            if (numerosLeer > 10) {
+                System.out.println("Error, el maximo es de 10 numeros.");
+            }
+        } while (numerosLeer > 10);
+        int numeros[] = new int[numerosLeer];
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Anota el numero " + i + ":");
+            numeros[i] = sc.nextInt();
+            for (int contador = 0; contador < i; contador++) {
+                if (numeros[i] == numeros[contador]) {
+                    System.out.println("Los numeros no pueden repetirse");
+                    i--;
                 }
             }
+        }
+        System.out.println("Introduce el numero que deseas buscar");
+        numBuscar = sc.nextInt();
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == numeros[numBuscar]) {
+                System.out.println("se ha encontrado el numero");;
             }
+        }
         Arrays.sort(numeros);
-        
-        for(int i=0;i<numeros.length;i++){
+
+        for (int i = 0; i < numeros.length; i++) {
             System.out.println(numeros[i]);
-            }
+        }
     }
-    
 }
